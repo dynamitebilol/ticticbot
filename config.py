98b3dc70dd37acc17a42a08e0a473b3dd50d4509
dynamitebilol <92247901+dynamitebilol@users.noreply.github.com>
2021-11-26 19:29:18 +0500

    db

--- config.py
+++ config.py
@@ -7,6 +7,3 @@ DB_USER = str(os.environ.get("DB_USER"))
 DB_PASS = str(os.environ.get("DB_PASS"))
 DB_NAME = str(os.environ.get("DB_NAME"))
 DB_HOST = str(os.environ.get("DB_HOST"))
-
-
-#"2127643889:AAHzOXJjncr0Rf3S6FXtm81q_477wfz9diY"
--- loader.py
+++ loader.py
@@ -1,9 +1,9 @@
 from postgresql import Database
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-import config
+from config import BOT_TOKEN
 
-bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
+bot = Bot(token=BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 db = Database()
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
